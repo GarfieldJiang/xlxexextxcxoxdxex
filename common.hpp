dee1adc89fd6ecf07e839ad3672ab40c8a095ffb
jehovah0121 <jehovah0121@gmail.com>
2017-04-12 11:17:04 +0800

    Regular expression matching: an elegant memoized DP solution
    - Integrate Catch for unit test.
    - Add .gitignore.

---
+++ common.hpp
@@ -0,0 +1,11 @@
+#ifndef __COMMON__
+#define __COMMON__
+
+#include <iostream>
+#include <string>
+#include <vector>
+#define CATCH_CONFIG_MAIN
+#include "catch/include/catch.hpp"
+using namespace std;
+
+#endif
---
+++ regular-expression-matching_v2.cpp
@@ -0,0 +1,54 @@
+#include "common.hpp"
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.length();
+        int n = p.length();
+        vector<vector<int> > dp = vector<vector<int> >();
+        for (int i = 0; i < m + 1; i++) {
+            dp.push_back(vector<int>());
+            for (int j = 0; j < n + 1; j++) {
+                dp[i].push_back(-1);
+            }
+        }
+
+        return isMatch(s, p, m, n, 0, 0, dp);
+    }
+
+    // dynamic programming - memoized recursion.
+    bool isMatch(const string& s, const string& p, int m, int n, int i, int j, vector<vector<int> >& dp) {
+        // cout << "Begin isMatch i = " << i << ", j = " << j << endl;
+        if (dp[i][j] >= 0) {
+            return dp[i][j];
+        }
+
+        if (j >= n) {
+            return dp[i][j] = i >= m;
+        }
+
+        if (j + 1 < n && p[j + 1] == '*') {
+            for (int matchLen = 0; matchLen <= m - i; matchLen++) {
+                if (matchLen != 0 && p[j] != '.' && p[j] != s[i + matchLen - 1]) {
+                    return dp[i][j] = 0;
+                }
+
+                if (isMatch(s, p, m, n, i + matchLen, j + 2, dp)) {
+                    return dp[i][j] = 1;
+                }
+            }
+        }
+
+        if (i >= m || (p[j] != '.' && p[j] != s[i])) {
+            return dp[i][j] = 0;
+        }
+
+        return dp[i][j] = isMatch(s, p, m, n, i + 1, j + 1, dp);
+    }
+};
+
+TEST_CASE("Regular expression matching v2", "[ALL]") {
+    Solution solution = Solution();
+    REQUIRE(solution.isMatch("aa", "a") == false);
+    REQUIRE(solution.isMatch("a", ".*..a*") == false);
+}
