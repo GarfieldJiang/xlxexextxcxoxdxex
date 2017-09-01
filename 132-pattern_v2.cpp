3063b547b55a52a62cc0a5281ebe030754aad674
Garfield <jehovah0121@gmail.com>
2017-09-01 16:02:36 +0800

    Create 132-pattern_v2.cpp

---
+++ 132-pattern_v2.cpp
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/132-pattern/description/
+
+class Solution {
+public:
+    // O(n log n) time.
+    bool find132pattern(vector<int>& nums) {
+        if (nums.size() == 0) {
+            return false;
+        }
+        
+        vector<int> mins;
+        vector<int> maxs;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (binarySearchForInclusion(nums[i], mins, maxs)) {
+                return true;
+            }
+            
+            int last = mins.size() - 1;
+            if (last < 0 || nums[i] < mins[last]) {
+                mins.push_back(nums[i]);
+                maxs.push_back(nums[i]);
+            } else if (nums[i] > maxs[last]) {
+                maxs[last] = nums[i];
+            }
+        }
+        
+        return false;
+    }
+    
+    bool binarySearchForInclusion(int num, vector<int>& mins, vector<int>& maxs) {
+        if (mins.size() == 0) {
+            return false;
+        }
+        
+        for (int beg = 0, end = mins.size(), mid = end / 2; beg < end; mid = beg + (end - beg) / 2) {
+            if (num < maxs[mid] && num > mins[mid]) {
+                return true;
+            }
+            
+            if (num >= mins[mid]) {
+                end = mid;
+            } else {
+                beg = mid + 1;
+            }
+        }
+        
+        return false;
+    }
+};
