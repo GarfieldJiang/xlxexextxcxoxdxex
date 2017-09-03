4a43f0547b06c7b69284553696a11f396020fb6a
Garfield <jehovah0121@gmail.com>
2017-09-03 20:29:29 +0800

    Create 132-pattern_v3.cpp
    Finally an O(n) solution after I got some hint.

---
+++ 132-pattern_v3.cpp
@@ -0,0 +1,77 @@
+// https://leetcode.com/problems/132-pattern/description/
+
+// I didn't think of reversely enumerating the input until a friend of mine hinted me...
+// This one finally runs in O(n) time.
+//
+// The idea is as follows: when you enumerate the input from the right side, you meet an
+// increasing subarray until you get a decreasing pair, when 'hasDecreasingPair' is set
+// to true. 
+//
+// - Before that, 'minValue' stores the minimum value met so far, and 'minIndex' the index
+//   'minValue' in the input.
+//
+// -  After that, we maintain 'maxValue' and 'minValue' so that they denote the biggest
+//    decreasing pair so far, until we found an input element less than 'minValue'.
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        if (nums.empty()) {
+            return false;
+        }
+        
+        bool hasDecreasingPair = false;
+        int minValue = nums[nums.size() - 1];
+        int maxValue = 0;
+        int minIndex = nums.size() - 1;
+        int maxIndex = -1;
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            if (!hasDecreasingPair) {
+                if (nums[i] < minValue) {
+                    minValue = nums[i];
+                    minIndex = i;
+                } else if (nums[i] > minValue) {
+                    hasDecreasingPair = true;
+                    maxValue = nums[i];
+                    maxIndex = i;
+                    updateMinValue(minIndex, minValue, maxValue, i + 1, nums);
+                }
+                continue;
+            }
+            
+            if (nums[i] < minValue) {
+                return true;
+            }
+
+            if (nums[i] == maxValue) {
+                int lastMaxIndex = maxIndex;
+                maxIndex = i;
+                for (int j = maxIndex + 1; j < lastMaxIndex; j++) {
+                    if (nums[j] > minValue) {
+                        minValue = nums[j];
+                    }
+                }
+            } else if (nums[i] > maxValue) {
+                minValue = maxValue;
+                maxValue = nums[i];
+                maxIndex = i;
+                updateMinValue(minIndex, minValue, maxValue, minIndex + 1, nums);
+            }
+        }
+        
+        return false;
+    }
+    
+private:
+    inline void updateMinValue(int& minIndex, int& minValue, int maxValue, int startIndex, vector<int>& nums) {
+        for (int j = startIndex; j < nums.size(); j++) {
+            if (nums[j] >= maxValue) {
+                break;
+            }
+
+            if (nums[j] > minValue) {
+                minValue = nums[j];
+                minIndex = j;
+            }
+        }
+    }
+};
