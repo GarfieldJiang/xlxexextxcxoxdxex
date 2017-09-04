25b1805da340dd66730f99d48727883a49614edf
Garfield <jehovah0121@gmail.com>
2017-09-04 10:59:42 +0800

    Create median-of-two-sorted-arrays_v2.cpp

---
+++ median-of-two-sorted-arrays_v2.cpp
@@ -0,0 +1,27 @@
+https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int totalSize = nums1.size() + nums2.size();
+
+        for (int i1 = 0, i2 = 0, currentUse1 = false, lastUse1 = false;
+             true;
+             i1 += currentUse1, i2 += !currentUse1, lastUse1 = currentUse1) {
+            
+            currentUse1 = i1 < nums1.size() && (i2 >= nums2.size() || nums2[i2] >= nums1[i1]);
+            
+            if (i1 + i2 < totalSize / 2) {
+                continue;
+            }
+            
+            double current = double(currentUse1 ? nums1[i1] : nums2[i2]);
+            if (totalSize % 2) {
+                return current;
+            } else {
+                double last = double(lastUse1 ? nums1[i1 - 1] : nums2[i2 - 1]);
+                return .5 * (current + last);
+            }
+        }
+    }
+};
