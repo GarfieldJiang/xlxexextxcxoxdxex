0893399e1649a78586012eb2409b9068efaea2a3
Garfield <jehovah0121@gmail.com>
2018-09-29 17:29:34 +0800

    Update reverse-integer.cpp

--- reverse-integer.cpp
+++ reverse-integer.cpp
@@ -3,34 +3,26 @@
 class Solution {
 public:
     int reverse(int x) {
+        if (x == -2147483648) {
+            return 0;
+        }
         bool negative = false;
         if (x < 0) {
             x = -x;
             negative = true;
         }
         
-        char digits[10] = {0};
-        int digitCount = 0;
-        for (int i = 0; i <= 9 && x != 0; i++, digitCount++) {
-            digits[i] = x % 10;
+        int y = 0;
+        int firstDigit = x % 10;
+        for (int i = 0; i <= 9 && x != 0; i++) {
+            int digit = x % 10;
+            if (i == 9 && firstDigit > 2) {
+                return 0;
+            }
+            y = y * 10 + (1 - 2 * negative) * digit;
             x /= 10;
         }
         
-        if (digitCount == 10 && digits[0] > 2) {
-            return 0;
-        }
-        
-        int y = 0;
-        int power = 1;
-        for (int i = digitCount - 1; i >= 0; i--) {
-            int val = digits[i] * power;
-            if (val < 0) {
-                y = 0;
-                break;
-            }
-            y += (1 - 2 * (int)negative) * val;
-            power *= 10;
-        }
         if (negative && y >= 0 || !negative && y < 0) {
             return 0;
         }
